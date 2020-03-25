c9d2dd30de14d0562fc2b5c33c664d650178ede9
pbailie <pbailie@users.noreply.github.com>
2020-03-25 17:59:35 -0400

    Update provision.pl
    Improving Vagrant VM

--- vagrant_provision/pl/provision.pl
+++ vagrant_provision/pl/provision.pl
@@ -29,10 +29,14 @@ my @HTML_PATH = (rootdir(), "var", "www", "html");
 my @APACHE_PATH = (rootdir(), "etc", "apache2");
 
 # Packages needed by OS
-my @REQUIRED_PACKAGES = ("apache2", "php", "php-db", "php-mysql", "mysql-server", "mysql-client", "lsb");
+my @REQUIRED_PACKAGES = ("apache2", "php", "php-gd", "php-db", "php-mysql", "mysql-server", "mysql-client", "lsb");
 
 # List of Flex LM binaries
 my @FLEXLM_FILES = ("adskflex", "lmgrd", "lmutil");
+my $FLEXLM_OWNER = "vagrant";
+my $FLEXLM_OWNER_UID = getpwnam $FLEXLM_OWNER;
+my $FLEXLM_OWNER_GID = getgrnam $FLEXLM_OWNER;
+my $FLEXLM_PERMISSIONS = 0700;
 
 # DB config
 my $DB_NAME = "phplw_dev";
@@ -46,7 +50,7 @@ my $CONF_FILE = "phplw.conf";
 my $UPDATE_CODE = "update_code.pl";
 
 # Vars and arrays
-my ($cmd, $source, $dest, $file, $files, $conf, @source_path, @dest_path, @working_path);
+my ($source, $dest, $file, $files, $conf, @source_path, @dest_path, @working_path);
 
 # ** -------------------------- END CONFIGURATION --------------------------- **
 
@@ -61,26 +65,29 @@ foreach (@REQUIRED_PACKAGES) {
 }
 
 # Copy Flex LM files to system.
-# TO DO: Some error handling if these files don't exist.
-#        Maybe we don't necessarily have to halt provisioning on error.
 @source_path = (@REPO_PATH, "vagrant_provision", "flex_lm");
 @dest_path   = @FLEXNETSERVER_PATH;
 
 $dest = catdir(@dest_path);
-if (!-e $dest) {
-    mkdir $dest, 0700;
-    print "Created directory: $dest\n";
-}
-
+mkdir $dest, 0701;
+print "Created directory: $dest\n";
 foreach (@FLEXLM_FILES) {
     $source = catfile(@source_path, $_);
     $dest   = catfile(@dest_path, $_);
-    if (-f $source) {
-        copy $source, $dest;
+
+    # autodie doesn't work with File::Copy
+    if (copy $source, $dest) {
         print "Copied Flex LM binary $_\n";
     } else {
-        print "Flex LM binary $_ NOT FOUND, NOT COPIED.\n";
+        print "Flex LM binary $_: $!\n";
+        exit 1;
     }
+
+    chown $FLEXLM_OWNER_UID, $FLEXLM_OWNER_GID, $dest;
+    print "$_ ownership granted to $FLEXLM_OWNER\n";
+
+    chmod $FLEXLM_PERMISSIONS, $dest;
+    print "$_ permissions set to ", sprintf("0%o\n", $FLEXLM_PERMISSIONS);
 }
 
 # Setup mysql
