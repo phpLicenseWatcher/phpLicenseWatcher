760307a782c606d7232377b016bf7db960d07bef
pbailie <pbailie@users.noreply.github.com>
2020-04-01 17:48:24 -0400

    Check Install Update
    - WIP for aesthetic improvement
    - changed red text to crimson
    - Improving code style
    - Removed references to obsolete functions

--- check_installation.php
+++ check_installation.php
@@ -1,15 +1,18 @@
 <?php
 
 require_once("common.php");
-print_header("Check PHPlicensewatcher installation");
 
+define("PASS_MARK", "<span style='color: green;'>&#10004; GOOD</span>");  // green heavy checkmark
+define("FAIL_MARK", "<span style='color: #dc143c;'>&#10006; MISSING</span>");  // red cross mark
+
+print_header();
 ?>
 
 <h1>Check PHPlicensewatcher installation</h1>
 <hr/>
 
 <p>This will check whether PHPlicensewatcher has been properly installed. This is not an exhaustive check but checks for common installation and configuration issues.</p>
-<p style="color: red;">When the script is finished with the check it will print out a congratulation message. If there is no congratulatory message a problem appeared that this script didn't anticipate :-(.</p>
+<p style="color: #dc143c;">When the script is finished with the check it will print out a congratulation message. If there is no congratulatory message a problem appeared that this script didn't anticipate :-(.</p>
 
 <?php
 
@@ -71,35 +74,19 @@ if ( is_executable( $lmutil_loc) ){
 }
 
 
-echo("<p>Verifying whether RRDtool is available:</p>\n ");
-
-if ( isset($rrdtool_bin) && is_executable($rrdtool_bin) ){
-	echo("<p class=\"ok\"><b>$rrdtool_bin</b> exists and is executable.</p>\n");
-} else {
-	echo("<p class=\"warning\">WARNING: RRDtool is not installed. If you want support please install RRDtool and set <tt>\$rrdtool_bin</tt> and <tt>\$rrd_dir</tt>.</p>\n");
-}
-
-echo("<p>Verifying whether the LUM i4blt (<b>$i4blt_loc</b>) is executable:</p>\n ");
-
-if ( is_executable( $i4blt_loc ."a") ){
-	echo("<p class=\"ok\"><b>$i4blt_loc</b> exists and is executable.</p>\n");
-}else{
-	echo("<p class=\"warning\">WARNING: $i4blt_loc It is not executable or there is some kind of issue. This is not a fatal error, but you will not be able to monitor LUM Servers. Please fix the <tt>\$i4blt_loc</tt> variable, check permissions or check whether file exists. (You can download a binary from <a href=\"http://www-306.ibm.com/software/awdtools/lum/component.html\">http://www-306.ibm.com/software/awdtools/lum/component.html</a>)</p>\n");
-}
-
 ###########################################################################
 # Check whether DB connection works
 ###########################################################################
 if ( isset($db_hostname) && isset($db_username) && isset($db_password) ) {
 
 	require_once 'DB.php';
-	
+
 	echo("<p>Checking DB connection: </p>");
-	
+
 	$db = DB::connect($dsn, true);
-	
+
 	if (DB::isError($db)) {
-	
+
 		echo("<p class=\"warning\">WARNING: DB connection failed because: " . $db->getMessage() . "<br/> Phplicensewatcher will work without a database connection, but you can only look at the current status - no long term statistics will be available.</p>
 		<p>Your current settings are</p>
 		<pre>
@@ -111,11 +98,11 @@ if ( isset($db_hostname) && isset($db_username) && isset($db_password) ) {
 		</pre>\n");
 	} else {
 		echo("<p class=\"ok\">DB connect succeeded.</p>\n");
-	}	
-	
+	}
+
 } else {
 		echo("<p class=\"warning\">WARNING: None of the DB settings set. You need to set \$db_hostname, \$db_username and \$db_password.</p>\n");
-	
+
 }
 
 echo("<h2 style=\"color: green;\">Congratulations all tests passed. This hopefully means :-) that PHPlicensewatcher is ready for use</h2>");
