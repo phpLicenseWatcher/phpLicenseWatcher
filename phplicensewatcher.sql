665858854ba63a70127cdf51e6bfb8e934c665f2
pbailie <pbailie@users.noreply.github.com>
2020-04-08 17:55:06 -0400

    Update phplicensewatcher.sql
    Schema adjustments per discussion in #8

--- phplicensewatcher.sql
+++ phplicensewatcher.sql
@@ -1,94 +1,120 @@
-
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-/*!40101 SET NAMES utf8 */;
+-- -----------------------------------------------------
+-- Schema php_license_watcher
+-- -----------------------------------------------------
 
-/*!40101 SET SQL_MODE=''*/;
+-- -----------------------------------------------------
+-- Table `feature`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `feature` ;
 
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE TABLE IF NOT EXISTS `feature` (
+  `featureID` BIGINT NOT NULL AUTO_INCREMENT,
+  `feature` VARCHAR(100) NULL DEFAULT NULL,
+  `showInLists` TINYINT NULL DEFAULT NULL,
+  `label` VARCHAR(100) NULL DEFAULT NULL,
+  PRIMARY KEY (`featureID`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 8
+DEFAULT CHARACTER SET = utf8;
 
 
-/*Table structure for table `feature` */
+-- -----------------------------------------------------
+-- Table `events`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `events` ;
 
-DROP TABLE IF EXISTS `feature`;
+CREATE TABLE IF NOT EXISTS `events` (
+  `date` DATE NOT NULL,
+  `time` TIME NOT NULL,
+  `type` VARCHAR(20) NOT NULL,
+  `feature` VARCHAR(40) NOT NULL,
+  `user` VARCHAR(80) NOT NULL,
+  `reason` TEXT NOT NULL,
+  PRIMARY KEY (`date`, `time`, `feature`, `user`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE `feature` (
-  `featureID` bigint(20) NOT NULL AUTO_INCREMENT,
-  `feature` varchar(100) DEFAULT NULL,
-  `showInLists` tinyint(4) DEFAULT NULL,
-  `label` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`featureID`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
-/*Table structure for table `flexlm_events` */
+-- -----------------------------------------------------
+-- Table `server`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `server` ;
 
-DROP TABLE IF EXISTS `flexlm_events`;
+CREATE TABLE IF NOT EXISTS `server` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50) NOT NULL,
+  `alias` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 12
+DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE `flexlm_events` (
-  `flmevent_date` date NOT NULL,
-  `flmevent_time` time NOT NULL,
-  `flmevent_type` varchar(20) NOT NULL,
-  `flmevent_feature` varchar(40) NOT NULL,
-  `flmevent_user` varchar(80) NOT NULL,
-  `flmevent_reason` text NOT NULL,
-  PRIMARY KEY (`flmevent_date`,`flmevent_time`,`flmevent_feature`,`flmevent_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Table structure for table `license_usage` */
+-- -----------------------------------------------------
+-- Table `usage`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `usage` ;
 
-DROP TABLE IF EXISTS `license_usage`;
+CREATE TABLE IF NOT EXISTS `usage` (
+  `server` INT NOT NULL,
+  `product` VARCHAR(80) NOT NULL,
+  `time` DATETIME NOT NULL,
+  `users` INT NOT NULL,
+  PRIMARY KEY (`product`, `server`, `time`),
+  INDEX `fk_usage_server1_idx` (`server` ASC),
+  CONSTRAINT `fk_usage_server1`
+    FOREIGN KEY (`server`)
+    REFERENCES `server` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE `license_usage` (
-  `flmusage_server` varchar(80) NOT NULL,
-  `flmusage_product` varchar(80) NOT NULL,
-  `flmusage_date` date NOT NULL,
-  `flmusage_time` time NOT NULL,
-  `flmusage_users` int(11) NOT NULL,
-  PRIMARY KEY (`flmusage_product`,`flmusage_server`,`flmusage_date`,`flmusage_time`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Table structure for table `licenses_available` */
+-- -----------------------------------------------------
+-- Table `available`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `available` ;
 
-DROP TABLE IF EXISTS `licenses_available`;
+CREATE TABLE IF NOT EXISTS `available` (
+  `date` DATE NOT NULL,
+  `server` INT NOT NULL,
+  `product` VARCHAR(80) NOT NULL,
+  `num_licenses` INT NOT NULL,
+  PRIMARY KEY (`date`, `product`, `num_licenses`, `server`),
+  INDEX `fk_available_server_idx` (`server` ASC),
+  CONSTRAINT `fk_available_server`
+    FOREIGN KEY (`server`)
+    REFERENCES `server` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE `licenses_available` (
-  `flmavailable_date` date NOT NULL,
-  `flmavailable_server` varchar(80) NOT NULL,
-  `flmavailable_product` varchar(80) NOT NULL,
-  `flmavailable_num_licenses` int(11) NOT NULL,
-  PRIMARY KEY (`flmavailable_date`,`flmavailable_server`,`flmavailable_product`,`flmavailable_num_licenses`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Table structure for table `server` */
+-- -----------------------------------------------------
+-- Table `status`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `status` ;
 
-DROP TABLE IF EXISTS `server`;
+CREATE TABLE IF NOT EXISTS `status` (
+  `id` BIGINT NOT NULL,
+  `dns` VARCHAR(100) NULL DEFAULT NULL,
+  `port` BIGINT NULL DEFAULT NULL,
+  `label` VARCHAR(100) NULL DEFAULT NULL,
+  `status` VARCHAR(10) NULL DEFAULT NULL,
+  `lm_hostname` VARCHAR(100) NULL DEFAULT NULL,
+  `isMaster` TINYINT NULL DEFAULT NULL,
+  `lmgrd_version` VARCHAR(20) NULL DEFAULT NULL,
+  `last_updated` DATETIME NULL DEFAULT NULL)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE `server` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `alias` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
-/*Table structure for table `server_status` */
-
-DROP TABLE IF EXISTS `server_status`;
-
-CREATE TABLE `server_status` (
-  `server_id` bigint(20) NOT NULL,
-  `server_dns` varchar(100) DEFAULT NULL,
-  `server_port` bigint(20) DEFAULT NULL,
-  `label` varchar(100) DEFAULT NULL,
-  `status` varchar(10) DEFAULT NULL,
-  `lm_hostname` varchar(100) DEFAULT NULL,
-  `isMaster` tinyint(4) DEFAULT NULL,
-  `lmgrd_version` varchar(20) DEFAULT NULL,
-  `last_updated` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
