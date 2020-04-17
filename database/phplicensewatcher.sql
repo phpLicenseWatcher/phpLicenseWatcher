10a901c55dd4cd497311425b2f296db371ea66b2
pbailie <pbailie@users.noreply.github.com>
2020-04-17 18:06:23 -0400

    Update phplicensewatcher.sql
    WIP

--- database/phplicensewatcher.sql
+++ database/phplicensewatcher.sql
@@ -7,38 +7,6 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL, NO_AUTO_VALUE_ON_ZERO';
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Table `features`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `features` ;
-
-CREATE TABLE IF NOT EXISTS `features` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `feature` VARCHAR(100) NULL DEFAULT NULL,
-  `show_in_lists` TINYINT NULL DEFAULT NULL,
-  `label` VARCHAR(100) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 8
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
--- Table `events`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `events` ;
-
-CREATE TABLE IF NOT EXISTS `events` (
-  `time` DATETIME NOT NULL,
-  `type` VARCHAR(20) NOT NULL,
-  `feature` VARCHAR(40) NOT NULL,
-  `user` VARCHAR(80) NOT NULL,
-  `reason` TEXT NOT NULL,
-  PRIMARY KEY (`time`, `feature`, `user`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
 -- Table `servers`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `servers` ;
@@ -53,6 +21,49 @@ CREATE TABLE IF NOT EXISTS `servers` (
   UNIQUE INDEX `unique_name_alias_isactive` (`name` ASC, `alias` ASC, `is_active` ASC))
 ENGINE = InnoDB
 AUTO_INCREMENT = 12
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `features`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `features` ;
+
+CREATE TABLE IF NOT EXISTS `features` (
+  `server_id` INT NOT NULL AUTO_INCREMENT,
+  `feature` VARCHAR(100) NOT NULL,
+  `show_in_lists` TINYINT NOT NULL,
+  `label` VARCHAR(100) NULL DEFAULT NULL,
+  PRIMARY KEY (`server_id`, `feature`),
+  CONSTRAINT `fk_features_servers`
+    FOREIGN KEY (`server_id`)
+    REFERENCES `servers` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 8
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `events`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `events` ;
+
+CREATE TABLE IF NOT EXISTS `events` (
+  `server_id` INT NOT NULL,
+  `feature` VARCHAR(40) NOT NULL,
+  `time` DATETIME NOT NULL,
+  `user` VARCHAR(80) NOT NULL,
+  `type` VARCHAR(20) NOT NULL,
+  `reason` TEXT NOT NULL,
+  PRIMARY KEY (`server_id`, `feature`, `time`, `user`),
+  CONSTRAINT `fk_events_servers`
+    FOREIGN KEY (`server_id`)
+    REFERENCES `servers` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
 
@@ -86,8 +97,8 @@ CREATE TABLE IF NOT EXISTS `available` (
   `server_id` INT NOT NULL,
   `date` DATE NOT NULL,
   `product` VARCHAR(80) NOT NULL,
-  `num_licenses` INT NOT NULL,
-  PRIMARY KEY (`server_id`, `date`, `product`, `num_licenses`),
+  `licenses` INT NOT NULL,
+  PRIMARY KEY (`server_id`, `date`, `product`),
   INDEX `fk_available_servers_idx` (`server_id` ASC),
   CONSTRAINT `fk_available_servers`
     FOREIGN KEY (`server_id`)
