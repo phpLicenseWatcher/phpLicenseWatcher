ccb7bc8fa4d488dac24f26c4655d26515ebd8a3f
Peter Bailie <pbailie@users.noreply.github.com>
2020-10-14 14:10:01 -0400

    Refactor
    Index.php and monitor_detail.php  copied from refactor branches to help with testing.  As requested by @mcglow2

--- index.php
+++ index.php
@@ -2,231 +2,92 @@
 
 require_once("common.php");
 require_once("tools.php");
-print_header("License Server Status");
-
-?>
-
-
-<link rel="top" href="index.php"/>
-</head>
-<body>
-<h1>License Server Status</h1>
-
-<hr/>
-<h2>Flexlm Servers</h2>
-
-
-<p>To get current usage for an individual server please click on the "Details" link next to the server. If you would like to get current usage for multiple servers on the same page use checkbox on the right of the server then click on the "Get current usage for multiple servers on one page".</p>
-
-<?php
-
-##########################################################################
-# We are using PHP Pear library to create tables :-)
-##########################################################################
-require_once ("HTML/Table.php");
+require_once("HTML/Table.php");
 
 $tableStyle = "class='table' ";
 
-# Create a new table object
+// Create a new table object
 $table = new HTML_Table($tableStyle);
-
 //$table->setColAttributes(1,"align=\"center\"");
 
-# Define a table header
+// Define a table header
 $headerStyle = "";
-$colHeaders = array("License port@server","Description", "Status", "Current Usage", "Available features/license","Master",  "lmgrd version");
+$colHeaders = array("License port@server", "Description", "Status", "Current Usage", "Available features/license", "lmgrd version");
 $table->addRow($colHeaders, $headerStyle, "TH");
 
-for ( $i = 0 ; $i < sizeof($servers) ; $i++ ) {
-	$data = run_command($lmutil_loc . " lmstat -c " . $servers[$i]);
-
-
-
+for ($i = 0 ; $i < sizeof($servers) ; $i++) {
+	$data = run_command("{$lmutil_loc} lmstat -c {$servers[$i]}");
 	$status_string = "";
-	$detaillink="<a href=\"details.php?listing=0&amp;server=" . $i . "\">Details</a>" ;
-	$listingexpirationlink="<a href=\"details.php?listing=1&amp;server=" . $i . "\">Listing/Expiration dates</a>" ;
+	$detaillink="<a href='details.php?listing=0&amp;server={$i}'>Details</a>";
+	$listingexpirationlink="<a href='details.php?listing=1&amp;server={$i}'>Listing/Expiration dates</a>";
 
-	foreach(explode(PHP_EOL, $data) as $line){
-		/* Look for an expression like this ie. kalahari: license server UP (MASTER) v6.1 */
-		if ( preg_match ("/: license server UP \(MASTER\)/i", $line) ) {
+	foreach(explode(PHP_EOL, $data) as $line) {
+		// Look for an expression like this ie. kalahari: license server UP (MASTER) v6.1
+        // preg_match() explicity returns int 1 on success.
+        switch (1) {
+        case preg_match ("/: license server UP \(MASTER\)/i", $line):
 			$status_string = "UP";
 			$class = "up";
 			$lmgrdversion = preg_replace("/.*license server UP \(MASTER\)/i ", "", $line);
-			$lmmaster = substr($line,0,strpos($line,':',0));
-		}else{
+            break;
+        case preg_match ("/: license server UP/i", $line):
+            $status_string = "UP";
+            $class = "up";
+            $lmgrdversion = preg_replace("/.*license server UP/i ", "", $line);
+        }
 
-                    if ( preg_match ("/: license server UP/i", $line) ) {
-                            $status_string = "UP";
-                            $class = "up";
-                            $lmgrdversion = preg_replace("/.*license server UP/i ", "", $line);
-                            $lmmaster = substr($line,0,strpos($line,':',0));
-                    }
+        switch(1) {
+        case preg_match("/Cannot connect to license server/i", $line, $out):
+        case preg_match("/Cannot read data/i", $line, $out):
+        case preg_match("/Error getting status/i", $line, $out):
+            $status_string = "DOWN";
+            $class = "down";
+            $lmgrdversion = "";
+            $detaillink="No details";
+            $listingexpirationlink = "";
+            break 2;
+        // Checking if vendor daemon has died even if lmgrd is still running
+        case preg_match("/vendor daemon is down/i", $line, $out):
+            $status_string = "VENDOR DOWN";
+            $class = "warning";
+            $lmgrdversion = preg_replace(".*license server UP \(MASTER\) /i", "", $line);
+            preg_replace(".*license server UP /i", "", $line);
+            break 2;
+        }
+    }
 
-                }
+    // If I don't get explicit reason that the server is UP set the status to DOWN
+    if ($status_string === "") {
+        $status_string = "DOWN";
+        $class = "down";
+        $lmgrdversion = "";
+        $detaillink = "No details";
+        $listingexpirationlink = "";
+    }
 
-		if ( preg_match ("/Cannot connect to license server/i", $line, $out) ) {
-			$status_string = "DOWN";
-			$class = "down";
-			$lmgrdversion = "" ;
-			$lmmaster = "";
-			$detaillink="No details";
-			$listingexpirationlink="";
-			break;
-		}
+    $table->AddRow(array(
+        $servers[$i],
+        $description[$i],
+        $status_string,
+        $detaillink,
+        $listingexpirationlink,
+        $lmgrdversion
+    ));
 
-		if ( preg_match ("/Cannot read data/i", $line, $out) ) {
-			$status_string = "DOWN";
-			$class = "down";
-			$lmgrdversion = "" ;
-			$lmmaster = "";
-			$detaillink="No details";
-			$listingexpirationlink="";
-			break;
-		}
-
-		if ( preg_match ("/Error getting status/i", $line, $out) ) {
-			$status_string = "DOWN";
-			$class = "down";
-			$lmgrdversion = "" ;
-			$lmmaster = "";
-			$detaillink="No details";
-			$listingexpirationlink="";
-			break;
-		}
-
-		/* Checking if vendor daemon has died evenif lmgrd is still running */
-		if ( preg_match ("/vendor daemon is down/i", $line, $out) ) {
-			$status_string = "VENDOR DOWN";
-			$class = "warning";
-			$lmgrdversion = preg_replace(".*license server UP \(MASTER\) /i", "", $line);
-			$lmmaster = substr($line,0,strpos($line,':',0));
-			preg_replace(".*license server UP /i", "", $line);
-			break;
-		}
-
-
-	}
-
-	/* If I don't get explicit reason that the server is UP set the status to DOWN */
-	if ( $status_string == "" ) {
- 		$status_string = "DOWN";
-		$class = "down";
-		$lmgrdversion = "" ;
-		$lmmaster = "";
-		$detaillink="No details";
-		$listingexpirationlink="";
-	}
-
-
-	$table->AddRow(array($servers[$i],$description[$i],$status_string,
-			 $detaillink,
-			 $listingexpirationlink,
-			 $lmmaster,
-			 $lmgrdversion));
-
-	# Set the background color of status cell
-	$table->updateCellAttributes( ($table->getRowCount() - 1) , 2, "class='" . $class . "'");
+	// Set the background color of status cell
+	$table->updateCellAttributes(($table->getRowCount() - 1) , 2, "class='{$class}'");
 	//$table->updateCellAttributes( 1 , 0, "");
-
-	# Close the pipe
-	//pclose($fp);
 }
 
-# Display the table
+// Output view.
+print_header();
+print <<< HTML
+<h1>License Server Status</h1>
+<hr />
+<h2>Flexlm Servers</h2>
+<p>To get current usage for an individual server please click on the "Details" link next to the server. If you would like to get current usage for multiple servers on the same page use checkbox on the right of the server then click on the "Get current usage for multiple servers on one page".</p>
+HTML;
+
 $table->display();
+print_footer();
 ?>
-
-
-<?php
-
-
-# Check whether we are monitoring LUM servers.
-if ( isset ($i4blt_loc) && $i4blt_loc != "" ) {
-
-echo ("<h2>LUM Servers</h2>");
-$tableStyle = "border=\"1\" cellpadding=\"1\" cellspacing=\"2\" ";
-
-# Create a new table object
-$table = new HTML_Table($tableStyle);
-
-$table->setColAttributes(1,"align=\"center\"");
-
-# Define a table header
-$headerStyle = "";
-$colHeaders = array("Server Name","Target ID", "Target Type", "Server Type", "Details","Status");
-$table->addRow($colHeaders, $headerStyle, "TH");
-
-
-  $fp = popen("TIMEOUT_FACTOR=2 ; export TIMEOUT_FACTOR ; " . $i4blt_loc . " -ls ", "r"); // FIXME: shell dependent code!
-   while ( !feof ($fp) ) {
-
-        $line = fgets ($fp, 1024);
-
-# If no license server is running, lum outputs:
-# ADM-10037: There are no active license servers
-		if ( preg_match ("/ADM-10037: There are no active license servers/i", $line) ) {
-			$servername = "" ;
-			$targetid = "" ;
-			$targettype = "" ;
-			$servertype = "" ;
-			$details = "" ;
-			$status_string = "DOWN";
-			$class="down" ;
-	$table->AddRow(array($servername,$targetid,$targettype,$servertype, $details, $status_string));
-	# Set the background color of status cell
-	$table->updateCellAttributes( ($table->getRowCount() - 1) , 5, "class='" . $class . "'");
-	$table->updateCellAttributes( 1 , 0, "");
-
-
-		}
-
-
-# If a license server is running, lum outputs something like:
-// ===========================================================================
-// ===                             S e r v e r s                           ===
-// ===========================================================================
-//
-//   Server Name:     ip:linux.site
-//   Target ID:       608cf8f8
-//   Target Type:     Linux
-//   Server Type:     Network
-//   Load:            Null or Not Available
-//   Load Threshold:  90
-//   Trace:           none
-//
-// ===========================================================================
-//
-//                           ==========================
-//                           === End of Server List ===
-//                           ==========================
-		if ( preg_match ("/Server Name:/i", $line) ) {
-                        $servername = preg_replace(".*Server Name:\ */i", "", $line);
-                        $targetid = preg_replace(".*Target ID:\ */i", "", fgets ($fp, 1024));  // Next line: TargetID
-                        $targettype = preg_replace(".*Target Type:\ */i", "", fgets ($fp, 1024));  // Next line: TargetType
-                        $servertype = preg_replace(".*Server Type:\ */i", "", fgets ($fp, 1024));  // Next line: ServerType
- 			$load = fgets ($fp, 1024);  // Next line: Load (not displayed)
-			$loadthreshold = fgets ($fp, 1024);  // Next line: Load Threshod (not displayed)
-			$trace = fgets ($fp, 1024);  // Next line: Trace (not displayed)
-			$details = "<a href=\"lumdetails.php?server=" . $servername . "\">Details</a>" ;
-			$status_string = "UP";
-			$class="up";
-	$table->AddRow(array($servername,$targetid,$targettype,$servertype, $details, $status_string));
-	# Set the background color of status cell
-	$table->updateCellAttributes( ($table->getRowCount() - 1) , 5, "class='" . $class . "'");
-	$table->updateCellAttributes( 1 , 0, "");
-
-
-		}
-
-
-}
-   pclose($fp);
-
-# Display the table
-$table->display();
-
-} // end if (
-?>
-
-
-<?php print_footer(); ?>
--- monitor_detail.php
+++ monitor_detail.php
@@ -1,15 +1,12 @@
 <?php
 
-require_once("./common.php");
-print_header("Per Feature License Monitoring");
-
+require_once("common.php");
+require_once("DB.php");
 
 $feature = preg_replace("/[^a-zA-Z0-9_|]+/", "", htmlspecialchars($_GET['feature'])) ;
 $label = $feature;
 
-require_once("DB.php");
 $db = DB::connect($dsn, true);
-
 if (DB::isError($db)) {
   die ($db->getMessage());
 }
@@ -26,124 +23,114 @@ if (DB::isError($recordset)) {
     die ($recordset->getMessage());
 }
 
-While ($row = $recordset->fetchRow()){
-
+// $row[0] = `name`, $row[1] = `label`
+while ($row = $recordset->fetchRow()){
    $label = $row[1];
-   if( $label == "" ){
-       $label =$row[0];
+   if (empty($label)) {
+       //`label` is NULL, so use `name`, instead.
+       $label = $row[0];
    }
-
-
 }
 
-
 $recordset->free();
-
 $db->disconnect();
-
-
 $label = str_replace('|', ' or ', $label);
 
-?>
-
-
-<h1><?php echo $label; ?> Usage</h1>
+// Print View.
+print_header();
+print <<<HTML
+<h1>{$label} Usage</h1>
 
 <hr/>
-<p class="a_centre">Data is taken every <?php echo($collection_interval); ?> minutes. It shows usage for past day, past week, past month and past year.</p>
+<p class="a_centre">Data is taken every {$collection_interval} minutes. It shows usage for past day, past week, past month and past year.</p>
 
-
-    <!--Load the AJAX API-->
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    <script type="text/javascript">
-
+<!--Load the AJAX API-->
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script type="text/javascript">
     // Load the Visualization API and the piechart package.
     google.charts.load('current', {'packages':['corechart']});
 
     // Set a callback to run when the Google Visualization API is loaded.
     google.charts.setOnLoadCallback(drawChart);
 
-
-      function drawChart() {
-          drawChart_day();
-          drawChart_week();
-          drawChart_month();
-          drawChart_year();
-      }
+    function drawChart() {
+        drawChart_day();
+        drawChart_week();
+        drawChart_month();
+        drawChart_year();
+    }
 
     function drawChart_day() {
-      var jsonData = $.ajax({
-          url: "graph_data.php?feature=<?php echo $feature; ?>&days=1",
-          dataType: "json",
-          async: false
-          }).responseText;
+        var jsonData = $.ajax({
+            url: "graph_data.php?feature={$feature}&days=1",
+            dataType: "json",
+            async: false
+        }).responseText;
 
-      // Create our data table out of JSON data loaded from server.
-      var data = new google.visualization.DataTable(jsonData);
+        // Create our data table out of JSON data loaded from server.
+        var data = new google.visualization.DataTable(jsonData);
 
-      // Instantiate and draw our chart, passing in some options.
-      var chart = new google.visualization.LineChart(document.getElementById('chart_div_day'));
-      chart.draw(data, {width: 1000, height: 400});
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.LineChart(document.getElementById('chart_div_day'));
+        chart.draw(data, {width: 1000, height: 400});
     }
 
     function drawChart_week() {
-      var jsonData = $.ajax({
-          url: "graph_data.php?feature=<?php echo $feature; ?>&days=7",
-          dataType: "json",
-          async: false
-          }).responseText;
+        var jsonData = $.ajax({
+            url: "graph_data.php?feature={$feature}&days=7",
+            dataType: "json",
+            async: false
+        }).responseText;
 
-      // Create our data table out of JSON data loaded from server.
-      var data = new google.visualization.DataTable(jsonData);
+        // Create our data table out of JSON data loaded from server.
+        var data = new google.visualization.DataTable(jsonData);
 
-      // Instantiate and draw our chart, passing in some options.
-      var chart = new google.visualization.LineChart(document.getElementById('chart_div_week'));
-      chart.draw(data, {width: 1000, height: 400});
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.LineChart(document.getElementById('chart_div_week'));
+        chart.draw(data, {width: 1000, height: 400});
     }
 
     function drawChart_month() {
-      var jsonData = $.ajax({
-          url: "graph_data.php?feature=<?php echo $feature; ?>&days=30",
-          dataType: "json",
-          async: false
-          }).responseText;
+        var jsonData = $.ajax({
+            url: "graph_data.php?feature={$feature}&days=30",
+            dataType: "json",
+            async: false
+        }).responseText;
 
-      // Create our data table out of JSON data loaded from server.
-      var data = new google.visualization.DataTable(jsonData);
+        // Create our data table out of JSON data loaded from server.
+        var data = new google.visualization.DataTable(jsonData);
 
-      // Instantiate and draw our chart, passing in some options.
-      var chart = new google.visualization.LineChart(document.getElementById('chart_div_month'));
-      chart.draw(data, {width: 1000, height: 400});
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.LineChart(document.getElementById('chart_div_month'));
+        chart.draw(data, {width: 1000, height: 400});
     }
 
     function drawChart_year() {
-      var jsonData = $.ajax({
-          url: "graph_data.php?feature=<?php echo $feature; ?>&days=365",
-          dataType: "json",
-          async: false
-          }).responseText;
+        var jsonData = $.ajax({
+            url: "graph_data.php?feature={$feature}&days=365",
+            dataType: "json",
+            async: false
+        }).responseText;
 
-      // Create our data table out of JSON data loaded from server.
-      var data = new google.visualization.DataTable(jsonData);
+        // Create our data table out of JSON data loaded from server.
+        var data = new google.visualization.DataTable(jsonData);
 
-      // Instantiate and draw our chart, passing in some options.
-      var chart = new google.visualization.LineChart(document.getElementById('chart_div_year'));
-      chart.draw(data, {width: 1000, height: 400});
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.LineChart(document.getElementById('chart_div_year'));
+        chart.draw(data, {width: 1000, height: 400});
     }
+</script>
 
-    </script>
-  </head>
-
-  <body>
-
-      <h2>Today</h2>
-    <div id="chart_div_day"></div>
-    <h2>Past Week</h2>
+<h2>Today</h2>
+<div id="chart_div_day"></div>
+<h2>Past Week</h2>
 <div id="chart_div_week"></div>
 <h2>Past Month</h2>
 <div id="chart_div_month"></div>
 <h2>Past Year</h2>
 <div id="chart_div_year"></div>
+HTML;
 
-<?php print_footer(); ?>
+print_footer();
+?>
