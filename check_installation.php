ce8d2dcbfabb1584488bb6525415ee4b0cc64520
Peter Bailie <pbailie@users.noreply.github.com>
2020-11-20 17:22:36 -0500

    update html_table.php
    html_table class (html_table.php) replaces pear/html_table.
    
    check_installation.php refactored to use html_table class.
    
    To Do: refactor other pages to use new html_table class.

--- check_installation.php
+++ check_installation.php
@@ -1,46 +1,52 @@
 <?php
 
 require_once "common.php";
-include_once "HTML/Table.php";
+include_once "html_table.php";
 
 define("PASS_MARK", "<span class='green-text'>&#10004; GOOD</span>");  // green heavy checkmark
 define("FAIL_MARK", "<span class='red-text'>&#10006; FAIL</span>");    // red cross mark
 define("REQUIRED_PHP", "7.0.0");
 
-// Parallel arrays
-$test_names   = array();
-$test_values  = array();
-$test_results = array();
+$table_attributes = array('id' => "install-check");
+$table_header = array("TEST", "VALUE", "RESULT");
+
+$table = new html_table($table_attributes);
+$table->set_header($table_header);
 
 // Calculate minimum php version id for first test.
 $ver = explode(".", REQUIRED_PHP);
 $minimum_php_version_id = $ver[0] * 10000 + $ver[1] * 100 + $ver[2];
 
-$test           = PHP_VERSION_ID >= $minimum_php_version_id;
-$test_names[]   = "PHP Version At Least " . REQUIRED_PHP;
-$test_values[]  = phpversion();
-$test_results[] = $test ? PASS_MARK : FAIL_MARK;
+$test         = PHP_VERSION_ID >= $minimum_php_version_id;
+$test_names   = "PHP Version At Least " . REQUIRED_PHP;
+$test_values  = phpversion();
+$test_results = $test ? PASS_MARK : FAIL_MARK;
+$table->add_row(array($test_names, $test_values, $test_results));
 
 // TO DO: Expand this test to validate config.php values.
-$test           = is_readable("config.php");
-$test_names[]   = "<code>config.php</code>";
-$test_values[]  = $test ? "Readable" : "Not Readable";
-$test_results[] = $test ? PASS_MARK : FAIL_MARK;
+$test         = is_readable("config.php");
+$test_names   = "<code>config.php</code>";
+$test_values  = $test ? "Readable" : "Not Readable";
+$test_results = $test ? PASS_MARK : FAIL_MARK;
+$table->add_row(array($test_names, $test_values, $test_results));
 
-$test           = extension_loaded("gd");
-$test_names[]   = "PHP Extension \"gd\" (required for graphs)";
-$test_values[]  = $test ? "Installed and Enabled" : "Not Found";
-$test_results[] = $test ? PASS_MARK : FAIL_MARK;
+$test         = extension_loaded("gd");
+$test_names   = "PHP Extension \"gd\" (required for graphs)";
+$test_values  = $test ? "Installed and Enabled" : "Not Found";
+$test_results = $test ? PASS_MARK : FAIL_MARK;
+$table->add_row(array($test_names, $test_values, $test_results));
 
-$test           = extension_loaded("xml");
-$test_names[]   = "PHP Extension \"xml\" (required by pear DB class)";
-$test_values[]  = $test ? "Installed and Enabled" : "Not Found";
-$test_results[] = $test ? PASS_MARK : FAIL_MARK;
+$test         = extension_loaded("xml");
+$test_names   = "PHP Extension \"xml\" (required by pear DB class)";
+$test_values  = $test ? "Installed and Enabled" : "Not Found";
+$test_results = $test ? PASS_MARK : FAIL_MARK;
+$table->add_row(array($test_names, $test_values, $test_results));
 
-$test           = isset($lmutil_binary) && is_executable($lmutil_binary);
-$test_names[]   = "<code>lmutil</code>";
-$test_values[]  = $test ? "Is Executable" : "Not Executable (maybe check permissions?)";
-$test_results[] = $test ? PASS_MARK : FAIL_MARK;
+$test         = isset($lmutil_binary) && is_executable($lmutil_binary);
+$test_names   = "<code>lmutil</code>";
+$test_values  = $test ? "Is Executable" : "Not Executable (maybe check permissions?)";
+$test_results = $test ? PASS_MARK : FAIL_MARK;
+$table->add_row(array($test_names, $test_values, $test_results));
 
 $test = (bool) function() {
     switch (false) {
@@ -59,22 +65,10 @@ $test = (bool) function() {
     $db->close();
     return true;
 };
-$test_names[]   = "Database Connectivity";
-$test_values[]  = $test ? "Connection OK" : "Connection Failed.";
-$test_results[] = $test ? PASS_MARK : FAIL_MARK;
-
-// Build rows for test results table.
-$test_table_rows = "";
-foreach ($test_names as $i => $test_name) {
-    $test_table_rows .= <<< HTML
-    <tr>
-        <td>{$test_name}</td>
-        <td>{$test_values[$i]}</td>
-        <td>{$test_results[$i]}</td>
-    </tr>
-
-HTML;
-}
+$test_names   = "Database Connectivity";
+$test_values  = $test ? "Connection OK" : "Connection Failed.";
+$test_results = $test ? PASS_MARK : FAIL_MARK;
+$table->add_row(array($test_names, $test_values, $test_results));
 
 // Display view.
 print_header();
@@ -85,14 +79,7 @@ print <<< HTML
 
 <p>This will check whether PHPlicensewatcher has been properly installed.
 This is not an exhaustive check but checks for common installation and configuration issues.</p>
-<table id='install-check'>
-    <tr>
-        <th>TEST</th>
-        <th>VALUE</th>
-        <th>RESULT</th>
-    </tr>
-{$test_table_rows}
-</table>
+{$table->get_html()}
 
 HTML;
 
--- html_table.php
+++ html_table.php
@@ -22,36 +22,42 @@ $html = $my_table->get_html();
 // Not implemented: colspan
 
 class html_table {
-    private $table_attrributes;
+    private $table_attributes;
+    private $caption;
     private $header;
     private $rows;
 
     public function __construct(array $table_attributes=null) {
-        $this->table_attributes = $this->set_attributes($table_attributes);
+        $this->table_attributes = $this->set_new_attributes($table_attributes);
+        $this->caption = new stdClass();
+        $this->set_caption();
         $this->header = new stdClass();
+        $this->set_header(array());
         $this->rows = array();
     }
 
-    public function set_header(array $content, string $caption=null, array $header_attributes=null, array $caption_attributes=null) {
-        $this->header->attributes = $this->set_attributes($header_attributes);
+    public function set_caption(string $content=null, array $attributes=null) {
+        $this->caption->attributes = $this->set_new_attributes($attributes);
+        $this->caption->html = $content;
+    }
+
+    public function set_header(array $content, array $header_attributes=null) {
+        $this->header->attributes = $this->set_new_attributes($header_attributes);
         $this->header->cells = array();
         foreach ($content as $cell_content) {
             $this->header->cells[] = new stdClass();
             $cell_index = array_key_last($this->header->cells);
-            $this->header->cells[$cell_index]->attributes = $this->set_attributes(null);
+            $this->header->cells[$cell_index]->attributes = $this->set_new_attributes(null);
             $this->header->cells[$cell_index]->html = $cell_content;
         }
-
-        $this->header->caption->attributes = $this->set_attributes($caption_attributes);
-        $this->header->caption->html = $caption;
     }
 
     public function set_header_cell(integer $col_offset=null, array $new_attributes=null, string $new_content=null) {
         $this->check_bounds($col_offset, $this->header->cells);
-        $this->change_cell($this->header->cells[$col_offset]), $new_attributes, $new_content);
+        $this->change_cell($this->header->cells[$col_offset], $new_attributes, $new_content);
     }
 
-    public function add_row(array $content, array $row_attributes) {
+    public function add_row(array $content, array $row_attributes=null) {
         $this->rows[] = new stdClass();
         $row_index = array_key_last($this->rows);
         $this->rows[$row_index]->attributes = $this->set_new_attributes($row_attributes);
@@ -64,26 +70,20 @@ class html_table {
         }
     }
 
-    public function set_row_cell(integer $row_offset, integer $col_offset, array $new_attributes, string $new_content) {
-        $this->check_bounds($row_offset, $this->rows);
-        $this->check_bounds($col_offset, $this->rows[$row_offset]->cells);
-        $this->change_cell($this->rows[$row_offset]->cells[$coll_offset], $new_attributes, $new_content);
+    public function set_row_col(int $row_offset, int $col_offset, array $new_attributes, string $new_content) {
+        $this->change_cell($this->rows[$row_offset]->cells[$col_offset], $new_attributes, $new_content);
     }
 
     public function get_rows_count() {
         return count($this->rows);
     }
 
-    public function get_html() {
+    public function get_cols_count($row_offset) {
+        return count($this->rows[$row_offset]->cells);
     }
 
-    private function check_bounds(integer &$offset, array $countable) {
-        switch (true) {
-        case is_null($offset):
-        case $offset < 0:
-        case $offset > (count($countable) - 1):
-            $offset = count($countable) - 1;
-        }
+    public function get_html() {
+        return $this->build_table();
     }
 
     private function set_new_attributes(array $attributes=null) {
@@ -100,45 +100,56 @@ class html_table {
         if (!is_null($new_content)) $cell->html = $new_content;
 
         // Do not change attribute when not set
-        if (isset($new_attributes['style'])) $cell->attributes->style = $new_attributes['style']
-        if (isset($new_attributes['class'])) $cell->attributes->class = $new_attributes['class']
-        if (isset($new_attributes['id']))    $cell->attributes->id    = $new_attributes['id']
-        if (isset($new_attributes['name']))  $cell->attributes->name  = $new_attributes['name']
+        if (isset($new_attributes['style'])) $cell->attributes->style = $new_attributes['style'];
+        if (isset($new_attributes['class'])) $cell->attributes->class = $new_attributes['class'];
+        if (isset($new_attributes['id']))    $cell->attributes->id    = $new_attributes['id'];
+        if (isset($new_attributes['name']))  $cell->attributes->name  = $new_attributes['name'];
     }
-
 
     private function build_table() {
-    }
+        $attributes = $this->build_attributes($this->table_attributes);
+        $html = "<table{$attributes}>\n";
 
+        if (!is_null($this->caption->html)) {
+            $attributes = $this->build_attributes($this->caption->attributes);
+            $html .= "<caption{$attributes}>{$this->caption->html}</caption>";
+        }
+
+        $html .= $this->build_header();
+        foreach($this->rows as $row) {
+            $html .= $this->build_row($row);
+        }
+
+        return $html .= "</table>\n";
+    }
 
     private function build_header() {
         $attributes = $this->build_attributes($this->header->attributes);
-        $html = "<tr{$attributes}>\n";
+        $html = "    <tr{$attributes}>\n";
         foreach($this->header->cells as $cell) {
             $attributes = $this->build_attributes($cell->attributes);
-            $html .= "<th{$attributes}>{$cell->html}</th>\n"
+            $html .= "        <th{$attributes}>{$cell->html}</th>\n";
         }
 
-        return $html . "</tr>\n";
+        return $html . "    </tr>\n";
     }
 
-    private function build_row(integer $row_index) {
-        $attributes = $this->build_attributes($this->rows[$row_index]->attributes);
-        $html = "<tr{$attributes}>\n";
-        foreach($this->rows[$row_index]->cells as $cell) {
+    private function build_row($row) {
+        $attributes = $this->build_attributes($row->attributes);
+        $html = "    <tr{$attributes}>\n";
+        foreach($row->cells as $cell) {
             $attributes = $this->build_attributes($cell->attributes);
-            $html .= "<td{$attributes}>{$cell->html}</td>\n"
+            $html .= "        <td{$attributes}>{$cell->html}</td>\n";
         }
 
-        return $html . "</tr>\n";
+        return $html . "    </tr>\n";
     }
 
-    private function build_attributes(array $attributes) {
+    private function build_attributes(stdClass $attributes) {
         $style = is_null($attributes->style) ? "" : " style='{$attributes->style}'";
         $class = is_null($attributes->class) ? "" : " class='{$attributes->class}'";
         $id    = is_null($attributes->id)    ? "" : " id='{$attributes->id}'";
         $name  = is_null($attributes->name)  ? "" : " name='{$attributes->name}'";
         return "{$name}{$id}{$class}{$style}";
     }
-
 }
