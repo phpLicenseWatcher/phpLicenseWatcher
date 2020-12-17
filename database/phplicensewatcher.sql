cb756159dd8434f0fb6d446af54f341ddb1d4556
Peter Bailie <pbailie@users.noreply.github.com>
2020-12-17 16:09:20 -0500

    Update phplicensewatcher.sql
    Improve server name check 'port@domain.tld' regex.
    All auto increments are unsigned.

--- database/phplicensewatcher.sql
+++ database/phplicensewatcher.sql
@@ -12,7 +12,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 DROP TABLE IF EXISTS `features` ;
 
 CREATE TABLE IF NOT EXISTS `features` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(100) NOT NULL,
   `label` VARCHAR(100) NULL DEFAULT NULL,
   `show_in_lists` TINYINT NOT NULL DEFAULT 0,
@@ -30,7 +30,7 @@ DEFAULT CHARACTER SET = utf8mb4;
 DROP TABLE IF EXISTS `servers` ;
 
 CREATE TABLE IF NOT EXISTS `servers` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(50) NOT NULL,
   `label` VARCHAR(100) NOT NULL,
   `is_active` TINYINT NOT NULL DEFAULT 0,
@@ -39,8 +39,8 @@ CREATE TABLE IF NOT EXISTS `servers` (
   `last_updated` DATETIME NULL DEFAULT now(),
   PRIMARY KEY (`id`),
   UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE,
-  CONSTRAINT `name_port@fqdn`
-    CHECK (REGEXP_LIKE(`name`, '^[0-9]{1,5}@[0-9a-zA-z\.\-]+\.[a-zA-Z\-]{2,}$')))
+  CONSTRAINT `port@domain.tld`
+    CHECK (REGEXP_LIKE(`name`, '^[0-9]{1,5}@([a-z0-9\-]+\.)+[a-z\-]{2,}$', 'i')))
 ENGINE = InnoDB
 AUTO_INCREMENT = 1
 DEFAULT CHARACTER SET = utf8mb4;
@@ -53,8 +53,8 @@ DROP TABLE IF EXISTS `licenses` ;
 
 CREATE TABLE IF NOT EXISTS `licenses` (
   `id` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `server_id` INT NOT NULL,
-  `feature_id` INT NOT NULL,
+  `server_id` INT UNSIGNED NOT NULL,
+  `feature_id` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `serverid_featureid_UNIQUE` (`server_id` ASC, `feature_id` ASC) VISIBLE,
   INDEX `fk_licenses_features1_idx` (`feature_id` ASC) VISIBLE,
